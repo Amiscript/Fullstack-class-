4516dc17e54d1f47e16690010d5e2fb3aa464c70
amiscript <edisonsime99@gmail.com>
2026-02-09 22:15:04 +0100

    class update

---
+++ script.js
@@ -0,0 +1,85 @@
+// data types in javascript
+// 1. string-  are just text   a. " "  double quote  .b ' ' single quote c. ` ` backslice
+// "name", '9', `true`
+
+// primitive data types
+
+// 2. numbers- 1,2,3,4,5....  float- 1.2, 2.3......decimals
+
+// 3. booleans- true / false
+
+// 4. null - empty, intentionally empty
+
+// 5. undefine- this means the either a value is not presented or the data type is not avaiable
+
+
+// non- primitive data types
+// 1. objects
+// 2.Arrays
+// 3.date
+
+
+//  variables
+
+// 1. let- this save a our data temporaraly and can be changed or redeclared
+
+// let variable name = data type - value
+// hoisting
+
+//  let firstName = "Edison";
+//   firstName = "David";
+//  console.log(firstName);
+
+
+//  let age = 9;
+//   age = 10
+//  console.log(age);
+
+
+
+//  let ismale = true;
+//  ismale = false
+//  console.log(ismale)
+
+
+
+// 2. const- this store our dataa permenantly and can not be change or redclared
+
+
+// const firstName = "David";
+// //   firstName = "David";
+//  console.log(firstName);
+
+
+//  const age = 10;
+// //   age = 10
+//  console.log(age);
+
+
+
+//  const ismale = false;
+// //  ismale = false
+// //  console.log(ismale)
+
+// string methods
+// 1.toUpperCase()
+// 2.toLowercase()
+ // 3. length
+
+//  let firstname = "david";
+//  console.log(firstname.toUpperCase())
+//  console.log(firstname.toLowerCase())
+// console.log(firstname.length)
+
+// assignment
+// write  programe thats store, your firstname, lastname, age, gender, phone Number, email, and display all in the browser
+
+// read up other strings method and number methods
+
+
+
+
+
+
+
+
